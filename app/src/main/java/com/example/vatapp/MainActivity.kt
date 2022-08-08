ea74210493546506415a0b68489bf9ed2407a63a
shaima <s.alghamdi@safcsp.org.sa>
2022-08-08 13:45:50 +0300

    function CalculateVat implemented

--- app/src/main/java/com/example/vatapp/MainActivity.kt
+++ app/src/main/java/com/example/vatapp/MainActivity.kt
@@ -1,23 +1,26 @@
 package com.example.vatapp
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
 import com.example.vatapp.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
-    // binding object
+
+    // TODO [1] Create binding object
     private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // TODO [2] Layout View Binding
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root // reference layout file
         setContentView(view) // link layout
 
-
+        // TODO [3] Button binding and Calculate VAT
         binding.btnCalculateVat.setOnClickListener {
             calculateVat()
         }// End setOnClickListener
@@ -26,7 +29,38 @@ class MainActivity : AppCompatActivity() {
     }// End onCreate
 
     private fun calculateVat() {
-        TODO("Not yet implemented")
+
+        // TODO [4] receive and save the inputs
+
+        // EditText value
+        val stringInTextField = binding.etTotalCost.text.toString()
+        val cost = stringInTextField.toDouble()
+
+        // Radio Group
+        val selectedId = binding.rgVatOption.checkedRadioButtonId
+
+        // Radio Button Selected
+        val vatPercentage = when(selectedId){
+            R.id.rb_vat_10 -> 0.10
+            R.id.rb_vat_15 -> 0.15
+            else -> 0.20
+        }
+
+        var vat = vatPercentage * cost
+
+        var total = cost + vat
+
+        // Switch Value - True or False
+        val roundVat = binding.switchRoundUp.isChecked
+
+        if (roundVat){
+            total = kotlin.math.ceil(total)
+        }
+
+        // Total Formatting
+        val formattedTotal = NumberFormat.getCurrencyInstance().format(total)
+        binding.txtCostTotal.text = getString(R.string.total_amount, formattedTotal)
+
     }
 
 }// End MainActivity
